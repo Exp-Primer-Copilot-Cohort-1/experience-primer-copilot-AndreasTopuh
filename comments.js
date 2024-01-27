a1d3d0f162512e9f053782ccd63ff0e8fcdb0c0f
AndreasTopuh <131517581+AndreasTopuh@users.noreply.github.com>
2024-01-27 09:22:51 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/about') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello');
-    } else {
-        res.statusCode = 404;
-        res.end();
-    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!');
 });
 
 server.listen(3000, 'localhost', () => {
     console.log('Server running at http://localhost:3000/');
 });
-
